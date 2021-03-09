1cca8cb81585f727ceedfa0369855445b8a5c455
Suyog-Raut <suyograut.jee@gmail.com>
2021-03-09 15:20:37 +0530

    9th test

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 
-var signincontent="You have succesfully signed up .";
-var failcontent="Oops! It seems that something went wrong. Please try again";
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -17,10 +15,7 @@ app.get("/register",(req,res)=>
 {
   res.render("register");
 });
-app.get("/join",(req,res)=>
-{
-  res.render("join");
-});
+
 app.get("/find",(req,res)=>
 {
   res.render("find");
@@ -37,22 +32,12 @@ app.get("/signin",(req,res)=>
 {
   res.render("signin");
 });
-app.get("/signup",(req,res)=>
-{
-  res.render("signup");
-});
+
 app.get("/dash",(req,res)=>
 {
   res.render("dash");
 });
-app.get("/success",(req,res)=>
-{
-  res.render("success", { content: signincontent });
-});
-app.get("/fail",(req,res)=>
-{
-  res.render("fail", { content: failcontent });
-});
+
 app.listen(3000,()=> {
   console.log("Server started on port 3000");
 });
